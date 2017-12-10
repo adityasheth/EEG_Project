45a82d244e7d1cfda13be39d05255e4a531f2a9a
adityasheth <adityasheth1994.as@gmail.com>
2017-12-09 18:14:45 -0600

    working code-versio_1

--- EEG_XGBoost.py
+++ EEG_XGBoost.py
@@ -11,17 +11,17 @@ import pandas as pd
 import os
 
 # Setting the working directory
-os.chdir("C:\\Users\\Aditya\\Desktop\\UTA Sems\\Sem_3\\Data Mining")
+os.chdir("E:\\Anger EEG\\EEG_project")
 
 
 # Importing the dataset
-dataset = pd.read_csv('FeatureMat_timeWin.csv', header = None)
-X = dataset.iloc[:, 0:1344].values # Feature vector
-y = dataset.iloc[:, 1344].values # Target variable
+dataset = pd.read_csv('Feature_Vector.csv', header = None)
+X = dataset.iloc[:, 0:31].values # Feature vector
+y = dataset.iloc[:, 31].values # Target variable
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.10, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -40,12 +40,13 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+Accuracy = (cm[0,0] + cm[1,1] + cm[2,2])/len(y_pred)
 
 # Applying k-fold cross validation
-from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X, y = y, cv = 10)
-accuracies.mean()
-accuracies.std()
+#from sklearn.model_selection import cross_val_score
+#accuracies = cross_val_score(estimator = classifier, X = X, y = y, cv = 10)
+#accuracies.mean()
+#accuracies.std()
 
 # Prnting the program runtime
 print(time.clock() - start_time, 'seconds')
